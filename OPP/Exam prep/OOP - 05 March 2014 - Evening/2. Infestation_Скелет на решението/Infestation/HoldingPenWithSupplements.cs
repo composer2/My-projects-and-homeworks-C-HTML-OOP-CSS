a93a87d3ddf6784954c52c9ccaddc1945f3f512a
composer2 <composer2be@gmail.com>
2016-07-05 16:01:25 +0300

    not throwing exception

---
+++ OPP/Exam prep/OOP - 05 March 2014 - Evening/2. Infestation_Скелет на решението/Infestation/HoldingPenWithSupplements.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infestation
+{
+   public class HoldingPenWithSupplements:HoldingPen
+    {
+        protected override void ExecuteAddSupplementCommand(string[] commandWords)
+        {
+        }
+    }
+}
