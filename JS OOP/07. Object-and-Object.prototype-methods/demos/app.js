6f743340684aa41b1b9c82adc09ada15c212603f
composer2 <composer2be@gmail.com>
2016-09-02 10:22:22 +0300

    exercises on demos

--- JS OOP/07. Object-and-Object.prototype-methods/demos/app.js
+++ JS OOP/07. Object-and-Object.prototype-methods/demos/app.js
@@ -1,38 +1,7 @@
+/* Object.prototype.valueOf() */
 (function () {
-    var o,
-        obj = {
-            prop: function () {
+    var o = new Object(),
+        myVar = o.valueOf();
 
-            },
-            foo: 'bar'
-        };
-    console.log(obj.foo);
-    obj.foo = 'baz';
-    console.log(obj.foo);
-
-    obj.jump = 'jumpy';
-    delete obj.prop;
-    console.log(obj.jump);
-
-    o = Object.seal(obj);
-    console.log(o === obj);
-    console.log(Object.isSealed(obj));
-
-    obj.foo = 'opa';
-    console.log(obj.foo);
-
-    //obj.duck = 'the friendly duck';
-    //console.log(obj.duck);
-    delete obj.foo;
-    console.log(obj);
-
-    function fail(){
-        'use strict';
-        delete obj.foo;
-        obj.spark='spark';
-    }
-    //fail();
-//Object.defineProperty(obj, 'ohai', {value: 17}); // throws a TypeError
-    Object.defineProperty(obj, 'foo', {value: 'eit'}); // changes existing property value
-    console.log(obj.foo);
-} ());
+    console.log('' + myVar); // [object Object]
+}());
